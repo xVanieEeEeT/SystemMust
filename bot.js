981161f4d79b248e2e86d4f6873b179c59c2beb2
xVanieEeEeT <42171095+xVanieEeEeT@users.noreply.github.com>
2018-10-26 21:21:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,12 +10,6 @@ client.on('ready', async () => {
   }, 1000);
   });
 
-  client.on('ready', async () => {
-    let guild = client.guilds.get("503072680040464404");
-    setInterval(function(){
-        guild.channels.find(a => a.id === "505163437463044096").setName(`.Members: [ ${guild.members.size} ]`);
-    }, 1000);
-    });
 
 
 const moment = require("moment")
